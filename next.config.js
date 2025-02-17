411f117f5a18bd496cb0e585abcf455c903cbcef
Atharv Shukla <38507802+Atharvshukla@users.noreply.github.com>
2025-02-17 23:24:30 +0530

    webpack removed

--- next.config.js
+++ next.config.js
@@ -13,20 +13,20 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        bufferutil: false,
-        'utf-8-validate': false,
-        fs: false,
-        net: false,
-        tls: false,
-        child_process: false,
-      };
-    }
-    return config;
-  },
+  // webpack: (config, { isServer }) => {
+  //   if (!isServer) {
+  //     config.resolve.fallback = {
+  //       ...config.resolve.fallback,
+  //       bufferutil: false,
+  //       'utf-8-validate': false,
+  //       fs: false,
+  //       net: false,
+  //       tls: false,
+  //       child_process: false,
+  //     };
+  //   }
+  //   return config;
+  // },
 };
 
 module.exports = nextConfig;
